9393f54fc569e1b9205bc42fd28cd43db6ece5f9
flyphisherman <48329146+flyphisherman@users.noreply.github.com>
2021-01-15 07:49:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD_ID = os.getenv('DISCORD_GUILD_ID')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
+    for guild in client.guilds:
+        if guild.id == GUILD_ID:
+            break
+
     print(f'{client.user} has connected to Discord!')
+    print(f'{guild.name}(id: {guild.id})\n')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if 'A wild bird appears!' in message.content:
+        response = '$bang'
+        await message.channel.send(response)
+
 
 client.run(TOKEN)
